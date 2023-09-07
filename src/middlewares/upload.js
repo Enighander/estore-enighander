91f55faee26fbe4be4cf4270cb48408b35fa97b8
Enighander <ringgo.enighander@gmail.com>
2023-09-07 14:43:59 +0700

    fix

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,7 +1,6 @@
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-<<<<<<< HEAD
   destination: function (req, file, cb) {
     cb(null, "src/temp/image");
   },
@@ -12,14 +11,6 @@ const storage = multer.diskStorage({
     if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG)$/)) {
       req.fileValidationError = "Only jpg and jpeg file type are allowed!";
       return cb(new Error("Only jpg and png file type are allowed!"), false);
-=======
-    destination: function (req, file, cb) {
-      cb(null, 'src/images')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix + '.png')
->>>>>>> origin/master
     }
     cb(null, true);
   },
